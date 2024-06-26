a9fc94da94b52e5b0e52d2474ac837724f79dc74
Ajith-kumar-1 <ajithkumar19092004@gmail.com>
2024-06-26 16:55:47 +0530

    day 5

---
+++ task 5/resume.js
@@ -0,0 +1,35 @@
+let  resume= {
+    "name": "Ajith kumar ",
+    "age": "19"
+    "email": "ajithkumar19092004@gmail.com",
+    "phone": "8248967426",
+    "location":"chennai",
+    "dob":"19.09.2004",
+    "gender":"male",
+
+    "education": [
+      {
+        "degree": "b.com(accounting and finance",
+        "university": "st.thomos arts and science",
+        "start_date": "2021",
+        "end_date": "2024",
+        "percentage": "78%"
+      },
+      {
+        "Higher Secondary": " p.a.k palanisami hr sec school ",
+        "start_date": "2017",
+        "end_date": "2019",
+        "percentage": "82%"
+      }
+    ],
+    "skills": [ 
+        "excel"
+        "accounts"
+    ],
+} 
+for (let i = 0; i <resume.lenth;i++){
+console.log(resume(i));
+
+
+
+};
